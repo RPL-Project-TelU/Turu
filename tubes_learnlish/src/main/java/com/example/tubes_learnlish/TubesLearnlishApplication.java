4018fbcb7132a154cc81423fba62d6d678b0ad6a
AnwarYusuf <55271533+anwar2307@users.noreply.github.com>
2022-07-02 11:25:06 +0700

    Add files via upload

---
+++ tubes_learnlish/src/main/java/com/example/tubes_learnlish/TubesLearnlishApplication.java
@@ -0,0 +1,13 @@
+package com.example.tubes_learnlish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubesLearnlishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubesLearnlishApplication.class, args);
+	}
+
+}
---
+++ tubes_learnlish/src/test/java/com/example/tubes_learnlish/TubesLearnlishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tubes_learnlish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TubesLearnlishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
